4712ddca801736e707f0fa4cd27534fa5e0a26a9
Ken Love <love121@gmail.com>
2013-05-15 15:49:04 -0400

    setup clipper defaults

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,32 +3,36 @@
 (function(document, location) {
   //we must use the same protocol as the target page
   // else, BE BLOCKED.
-  var dev = true
-    , protocol = location.protocol
-    , domain = dev ? 'github.com' : 'dev.tgdcom.com'
-    , path = dev ? '/freality/clippercode/raw/master' : '/clipmaker'
-    , uri = protocol + '//' + domain + path;
+  var dev = true;
+  var settings = {}
+    , setting.dev = dev
+    , settings.protocol = location.protocol
+    , settings.domain = dev ? 'github.com' : 'dev.tgdcom.com'
+    , settings.path = dev ? '/freality/clippercode/raw/master' : '/clipmaker'
+    , settings.url = url = protocol + '//' + domain + path;
   
   cs = document.createElement('LINK');
   cs.rel = 'stylesheet';
-  cs.href = uri + '/css/styles.css';
+  cs.href = url + '/css/styles.css';
   cs.media = 'all';
   cs.type = 'text/css';
   document.getElementsByTagName('head')[0].appendChild(cs);
   
   qs = document.createElement('SCRIPT');
   qs.type = 'text/javascript'; 
-  qs.src = uri + '/js/qwery.min.js';
+  qs.src = url + '/js/qwery.min.js';
   document.getElementsByTagName('head')[0].appendChild(qs);
   
   zs = document.createElement('SCRIPT');
   zs.type = 'text/javascript';
-  zs.src = uri + '/js/ZeroClipboard.min.js';
+  zs.src = url + '/js/ZeroClipboard.min.js';
   document.getElementsByTagName('head')[0].appendChild(zs);
   
   ms = document.createElement('SCRIPT');
   ms.type = 'text/javascript';
-  ms.src = uri + '/js/main.js';
+  ms.src = url + '/js/main.js';
   document.getElementsByTagName('head')[0].appendChild(ms);
+  
+  window.ClipperSettings = settings;
   
 })(this.document, window.location);
---
+++ js/main.js
@@ -0,0 +1,16 @@
+(function($, qwery, ZeroClipboard, document, location) {
+  var settings = window.ClipperSettings || {};
+  var url = settings ? settings.url : "";
+  var movie_path = url + "ZeroClipboard.swf";
+  
+  var clip_defaults = {
+    moviePath:         movie_path,                 // URL to movie
+    trustedDomains:    undefined,                  // Domains that we should trust (single string or array of strings)
+    hoverClass:        "zeroclipboard-is-hover",   // The class used to hover over the object
+    activeClass:       "zeroclipboard-is-active",  // The class used to set object active
+    allowScriptAccess: "sameDomain"                // SWF outbound scripting policy
+  };
+  
+  ZeroClipboard.setDefaults(clip_defaults);
+  
+})(jQuery, qwery, ZeroClipboard, this.document, window.location);
